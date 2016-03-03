47a741dfa757f369cedd7e7e092eb55f327d8d15
Mauro Junior <mauropereirajr@gmail.com>
2016-03-03 09:09:25 -0300

    exposed mongodb driver on public API

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
+const MongoObserver = require('mongo-observer');
 
 module.exports = {
     Publisher: require('./lib/publisher'),
-    Subscriber: require('./lib/subscriber')
+    Subscriber: require('./lib/subscriber'),
+    getMongoConnection: MongoObserver.getMongoConnection,
+    ObjectID: MongoObserver.ObjectID
 };
